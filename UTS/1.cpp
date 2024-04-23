d024234dbfc8133cc92368f624cee27a6330a54d
alissyaa <alisssyahumairah@gmail.com>
2024-04-23 09:43:34 +0700

    uts

--- UTS/1.cpp
+++ UTS/1.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
-
-void Dibalik(string str) {
-    for (int i = str.length() - 1; i >= 0; i--) {
-        cout << str[i] << endl;
-    }
-}
 
 int main() {
     string kalimat;
     cout << "Masukkan sebuah kalimat: ";
     getline(cin, kalimat);
 
-    Dibalik(kalimat);
-
+    for (int i = kalimat.length() - 1; i >= 0; i--) {
+    cout << kalimat[i] << endl;
+  }
     return 0;
 }
